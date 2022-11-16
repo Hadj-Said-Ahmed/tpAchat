be88c9579c3cf5dfaf79ac0cecadc4d61ae0ee8d
Hadj-Said-Ahmed <ahmed.hadjsaid@esprit.tn>
2022-11-16 17:12:20 +0100

    maj 16/11/2022

--- src/test/java/com/esprit/examen/services/CategoryServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/CategoryServiceImplTest.java
@@ -1,4 +1,4 @@
-/*package com.esprit.examen.services;
+package com.esprit.examen.services;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -74,4 +74,3 @@ public class CategoryServiceImplTest {
 	}
 	
 }
-*/
