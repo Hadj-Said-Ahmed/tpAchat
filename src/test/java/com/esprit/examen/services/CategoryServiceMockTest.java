92cb93ea18443bc8056d69a416c4e7edbf4c060d
Hadj-Said-Ahmed <79025213+Hadj-Said-Ahmed@users.noreply.github.com>
2022-11-18 10:59:49 +0100

    Update CategoryServiceMockTest.java

--- src/test/java/com/esprit/examen/services/CategoryServiceMockTest.java
+++ src/test/java/com/esprit/examen/services/CategoryServiceMockTest.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+/*package com.esprit.examen.services;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -35,3 +35,4 @@ public class CategoryServiceMockTest {
 	}
 
 }
+*/
